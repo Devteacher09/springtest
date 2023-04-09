f91b70147bae90a75f493a81f3687ed4b1dc6daa
test <test@test.com>
2023-04-09 16:53:06 +0900

    boardmapping

--- HelloSpring/src/main/java/com/bs/spring/board/model/vo/Board.java
+++ HelloSpring/src/main/java/com/bs/spring/board/model/vo/Board.java
@@ -1,6 +1,9 @@
 package com.bs.spring.board.model.vo;
 
 import java.sql.Date;
+import java.util.List;
+
+import com.bs.spring.member.model.vo.Member;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,12 +18,12 @@ public class Board {
 	
 	private long boardNo;
 	private String boardTitle;
-	private String boardWriter;
-//	private Member boardWriter;
+	//private String boardWriter;
+	private Member boardWriter;
 	private String boardContent;
 	private Date boardDate;
 	private int boardReadCount;
-//	private List<Attachment> files;
+	private List<Attachment> files;
 }
 
 
