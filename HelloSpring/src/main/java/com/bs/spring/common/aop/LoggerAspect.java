c79e998a4f7cbe73a32a234989583bc922384aaf
test <test@test.com>
2023-04-09 12:16:02 +0900

    aop

---
+++ HelloSpring/src/main/java/com/bs/spring/common/aop/LoggerAspect.java
@@ -0,0 +1,29 @@
+package com.bs.spring.common.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+
+import lombok.extern.slf4j.Slf4j;
+
+//로그를 출력해주는 공통기능
+@Slf4j
+public class LoggerAspect {
+	
+	//공통으로 실행할 메소드를 선언
+	//메소드를 선언할때는 선언방식이 advisor에 따라 정해져있음
+	//before advisor등록하기
+	
+	public void loggerBefore(JoinPoint jp) {
+		log.debug("===== before logger 실행 =====");
+		//jp를 이용해서 실행하는 메소드, 클래스명 출력하기
+		Signature sig=jp.getSignature();
+		log.debug(sig.getDeclaringTypeName()+" : "+sig.getName());
+		log.debug("===============================");
+	}
+	
+}
+
+
+
+
+
